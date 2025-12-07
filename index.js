838edfe9f0577933718964f1c8aafe53dfeccb19
HaR-S-H <h8551283@gmail.com>
2025-12-07 22:42:39 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
     {
-        origin: "http://localhost:3000",
+        origin: process.env.ORIGIN || "*",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     }
